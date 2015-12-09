39c89af0b5be973317786c3d37728f0864202610
Yaro Kifor <yarokifor@gmail.com>
2015-12-09 18:50:02 -0500

    Fixed indents in ping.py

--- ping.py
+++ ping.py
@@ -23,20 +23,20 @@ class Ping:
         GPIO.setup(self.echoPin, GPIO.IN)
     
     def distance(self):
-		#Trigger the sensor
+        #Trigger the sensor
         GPIO.output(self.triggerPin, GPIO.HIGH)
         time.sleep(0.01)
         GPIO.output(self.triggerPin, GPIO.LOW)
-		#Wait for raising edge
+        #Wait for raising edge
         while(GPIO.input(self.echoPin)==0):
             a=0
-		#Get epoch time
+        #Get epoch time
         timeLength=time.time()
         #Wait for falling edge
-		while(GPIO.input(self.echoPin)==1):
+        while(GPIO.input(self.echoPin)==1):
             a=1
-		#Get how long the echoPin was high for
+        #Get how long the echoPin was high for
         timeLength=time.time()-timeLength
-		#Return in meters what sensor deteced
-		# length of echo * 340 (speed of sound) / 2 (half the distance)
+        #Return in meters what sensor deteced
+        # length of echo * 340 (speed of sound) / 2 (half the distance)
         return timeLength / 170
