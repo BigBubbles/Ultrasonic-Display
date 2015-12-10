4a7532daae34b9f7ed995afd9e44d8030177ca6b
Yaro Kifor <yarokifor@gmail.com>
2015-12-10 07:27:10 -0500

    Added a timeout to the distance function in the Ping class.

--- ping.py
+++ ping.py
@@ -11,12 +11,18 @@ by Yaro Kifor
 import time
 import RPi.GPIO as GPIO
 
+#Exception for if a callback is called
+class CallbackCalled(Exception):
+    pass
+        
 class Ping:
     triggerPin=0
     echoPin=0
-    def __init__(self, triggerPin, echoPin, pinMode=GPIO.BCM):
+    def init(self, triggerPin, echoPin, pinMode=GPIO.BCM):
+        #Set private varibles
         self.triggerPin=triggerPin
         self.echoPin=echoPin
+        #Setup pin and set out put pins to low
         GPIO.setmode(pinMode)
         GPIO.setup(self.triggerPin, GPIO.OUT)
         GPIO.output(self.triggerPin, GPIO.LOW)
@@ -28,15 +34,60 @@ class Ping:
         time.sleep(0.0001)
         GPIO.output(self.triggerPin, GPIO.LOW)
         #Wait for raising edge
-        while(GPIO.input(self.echoPin)==0):
-            a=0
+        GPIO.wait_for_edge(self.echoPin,GPIO.RISING)
         #Get epoch time
-        timeLength=time.time()
+        echoLength=time.time()
         #Wait for falling edge
-        while(GPIO.input(self.echoPin)==1):
-            a=1
+        GPIO.wait_for_edge(self.echoPin,GPIO.FALLING)
         #Get how long the echoPin was high for
-        timeLength=time.time()-timeLength
+        echoLength=time.time()-echoLength
         #Return in meters what sensor deteced
         # length of echo * 343.2 (speed of sound in m/s) / 2 (half the distance) * 100 (for cm)
-        return timeLength * 171600 
+        return echoLength * 171600
+    
+    #Needs testing
+    def distance(self, timeout=0):
+        #If timeout is 0 then timeout is forever
+        if timeout == 0:
+            #Trigger the sensor
+            GPIO.output(self.triggerPin, GPIO.HIGH)
+            time.sleep(0.0001)
+            GPIO.output(self.triggerPin, GPIO.LOW)
+            #Wait for raising edge
+            GPIO.wait_for_edge(self.echoPin,GPIO.RISING)
+            #Get epoch time
+            echoLength=time.time()
+            #Wait for falling edge
+            GPIO.wait_for_edge(self.echoPin,GPIO.FALLING)
+            #Get how long the echoPin was high for
+            echoLength=time.time()-echoLength
+            #Return in meters what sensor deteced
+            # length of echo * 343.2 (speed of sound in m/s) / 2 (half the distance) * 100 (for cm)
+            return echoLength * 171600
+        else
+            echoLength=0
+        
+            #function for calling back
+            def echoFalling():
+                #Sets echoLength to how echoPin was high
+                echoLength=echoLength-time.time()
+                #Remove event callback it doesn't need to be called again
+                GPIO.remove_event_callback(self.echoPin)
+                raise Callbackcalled()
+        
+            try:            
+                #Add the event for the call back
+                return GPIO.add_event_detect(echoPin,GPIO.FALLING,callback=echoFalling)
+                GPIO.output(self.triggerPin, GPIO.HIGH)
+                time.sleep(0.0001)
+                GPIO.output(self.triggerPin, GPIO.LOW)
+                #Get epoch time
+                echoLength=time.time()
+                #Wait for the timeout
+                time.sleep(timeout)
+                #Remove event callback
+                GPIO.remove_event_callback(self.echoPin)
+                #Return -1 as failure
+                return -1
+            except CallbackCalled:
+                return echoLength * 171600 
