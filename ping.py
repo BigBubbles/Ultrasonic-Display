6c1f464a0a7b875d632c8b653cced74473b000fe
Yaro Kifor <yarokifor@gmail.com>
2015-12-10 23:31:55 -0500

    Reworked and fixed ping class.

--- ping.py
+++ ping.py
@@ -27,32 +27,21 @@ class Ping:
         GPIO.setup(self.triggerPin, GPIO.OUT)
         GPIO.output(self.triggerPin, GPIO.LOW)
         GPIO.setup(self.echoPin, GPIO.IN)
-    
-    def distance(self):
-        #Trigger the sensor
+
+    def triggerSensor(self):
         GPIO.output(self.triggerPin, GPIO.HIGH)
         time.sleep(0.0001)
         GPIO.output(self.triggerPin, GPIO.LOW)
-        #Wait for raising edge
-        GPIO.wait_for_edge(self.echoPin,GPIO.RISING)
-        #Get epoch time
-        echoLength=time.time()
-        #Wait for falling edge
-        GPIO.wait_for_edge(self.echoPin,GPIO.FALLING)
-        #Get how long the echoPin was high for
-        echoLength=time.time()-echoLength
-        #Return in meters what sensor deteced
-        # length of echo * 343.2 (speed of sound in m/s) / 2 (half the distance) * 100 (for cm)
-        return echoLength * 171600
+        
+    def echoFalling():
+        #Raise CallbackCalled to be catched
+        raise Callbackcalled()
     
     #Needs testing
     def distance(self, timeout=0):
         #If timeout is 0 then timeout is forever
         if timeout == 0:
-            #Trigger the sensor
-            GPIO.output(self.triggerPin, GPIO.HIGH)
-            time.sleep(0.0001)
-            GPIO.output(self.triggerPin, GPIO.LOW)
+            tiggerSensor()
             #Wait for raising edge
             GPIO.wait_for_edge(self.echoPin,GPIO.RISING)
             #Get epoch time
@@ -64,23 +53,12 @@ class Ping:
             #Return in meters what sensor deteced
             # length of echo * 343.2 (speed of sound in m/s) / 2 (half the distance) * 100 (for cm)
             return echoLength * 171600
-        else
+        else:
             echoLength=0
-        
-            #function for calling back
-            def echoFalling():
-                #Sets echoLength to how echoPin was high
-                echoLength=echoLength-time.time()
-                #Remove event callback it doesn't need to be called again
-                GPIO.remove_event_callback(self.echoPin)
-                raise Callbackcalled()
-        
             try:            
                 #Add the event for the call back
-                return GPIO.add_event_detect(echoPin,GPIO.FALLING,callback=echoFalling)
-                GPIO.output(self.triggerPin, GPIO.HIGH)
-                time.sleep(0.0001)
-                GPIO.output(self.triggerPin, GPIO.LOW)
+                GPIO.add_event_detect(echoPin,GPIO.FALLING,callback=echoFalling)
+                tiggerSensor()
                 #Get epoch time
                 echoLength=time.time()
                 #Wait for the timeout
@@ -90,4 +68,8 @@ class Ping:
                 #Return -1 as failure
                 return -1
             except CallbackCalled:
-                return echoLength * 171600 
+                #Sets echoLength to how echoPin was high
+                echoLength=echoLength-time.time()
+                #Remove event callback it doesn't need to be called again
+                GPIO.remove_event_callback(self.echoPin)
+                return echoLength * 171600
